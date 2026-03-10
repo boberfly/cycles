a0245fea3dc7cc58f81919ee106fe86069c592e8
sherholz <sebastian.herholz@gmail.com>
2026-03-10 16:36:49 +0100

    Adding missing changes

--- src/graph/node_type.h
+++ src/graph/node_type.h
@@ -149,7 +149,7 @@ struct NodeType {
   static thread_mutex node_type_mutex_;
 
 #define NODE_DEFINE(structname) \
-  const NodeType *structname::node_type_ = get_node_type(); \
+  const NodeType *structname::node_type_ = nullptr; \
   thread_mutex structname::node_type_mutex_; \
   unique_ptr<Node> structname::create(const NodeType *) \
   { \
